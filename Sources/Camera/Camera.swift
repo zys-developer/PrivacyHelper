9d77057d24bcec528d2ddb09993469a998bacbe3
zys <zys_dev@163.com>
2021-08-18 15:33:24 +0800

    添加相册权限、相机权限

---
+++ Sources/Camera/Camera.swift
@@ -0,0 +1,8 @@
+//
+//  Camera.swift
+//  Pods-PrivacyHelper_Example
+//
+//  Created by 张先生 on 2021/8/18.
+//
+
+import Foundation
---
+++ Sources/PhotoLibrary/PhotoLibrary.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoLibrary.swift
+//  Pods-PrivacyHelper_Example
+//
+//  Created by 张先生 on 2021/8/18.
+//
+
+import Foundation
